ee7b57cc66dc382a2e0cf8d442189b33b01d8c66
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-02-18 14:13:52 +0100

    Initialize colors in tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,30 @@
 module.exports = {
-  theme: {},
+  theme: {
+    colors: {
+      blue: {
+        100: '#0410F1',
+        70: '#4F58F5',
+        30: '#B4B7FB',
+        10: '#E6E7FE',
+      },
+      yellow: {
+        100: '#F8E51C',
+        70: '#FAED60',
+        30: '#FDF7BB',
+        10: '#FEFCE8'
+      },
+      orange: {
+        100: '#FB4D0E',
+        70: '#FC8256',
+        30: '#FECAB7',
+        10: '#FFEDE7'
+      },
+      black: '#141414',
+      gray: {
+        70: '#5B5B5B',
+        30: '#B9B9B9',
+        10: '#E7E7E7'
+      }
+    }
+  },
 };
