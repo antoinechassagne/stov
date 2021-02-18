90145b74a781055214bfc6bcd91d10f74f069ef1
Paul Riffard <paul.riffard@gmail.com>
2021-02-18 16:34:41 +0100

    create colloc

---
+++ client/components/ColocForm.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <h2>Create Coloc Form work</h2>
+    <form>
+    <label>Rentrer le nom de votre coloc</label>
+    <input type="text" v-model="colocName">
+    <button v-if="colocName" @click.prevent="successCreate()" > Créer la collocation </button> 
+  </form>
+
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+  return{
+    colocName:"",
+  }
+
+},
+methods:{
+  successCreate(){
+
+    this.$emit("trigger-succes")
+
+  }
+}
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/components/ColocSuccess.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h2>Create Coloc Succes work</h2>
+  <div>
+    <p>Bravo vous avez créer votre collocation n'hésitez pas a le partager grace a ce lien incroyable</p>
+  <div>Ceci est le lien incroyable</div> <button>copier le lien</button>
+  </div>
+  <button  > Maintenant crée votre premiere tâches </button>
+
+</div>
+
+</template>
+
+<script>
+export default {
+
+  methods:{
+    createTask(){
+       this.$router.push("/tasks/create");
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
