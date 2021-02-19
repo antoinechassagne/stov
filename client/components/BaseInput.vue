68748cec5483d483401e72d641e4094a26890a39
augcouec <augcouec@gmail.com>
2021-02-19 16:00:09 +0100

    fin du register lack button

---
+++ client/components/BaseInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col m-5 mt-3">
+    <label class="mt-2 m">{{ label }}</label>
+    <input class="border-2 p-2 mt-3 border-gray-30 rounded-md" :placeholder="placeholder" :type="type" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    label: {},
+    placeholder: {},
+    type: {},
+  },
+};
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,16 +1,35 @@
 <template>
   <fragment>
-    <Heading level="1">S'inscrire</Heading>
+    <div class="min-h-screen flex bg-gray-50">
+      <div class="max-w-md mx-auto">
+        <div>
+          <img class="mx-auto w-100" src="../assets/images/signUpBackground.png" alt="Workflow" />
+        </div>
+        <div class="rounded-t-xl bg-white w-full h-screen translate-top">
+          <h4 class="text-center pt-10 text-2xl font-bold">Créer ton profil !</h4>
+          <BaseInput label="Choisir un pseudo" placeholder="john" type="text"></BaseInput>
+          <BaseInput label="Entre ton addresse email" placeholder="john@john.com" type="mail"></BaseInput>
+          <BaseInput label="Choisis un mot de passe" placeholder="" type="Password"></BaseInput>
+        </div>
+      </div>
+    </div>
   </fragment>
 </template>
 
 <script>
 import Heading from "@/components/texts/Heading";
+import BaseInput from "@/components/BaseInput";
 
 export default {
   name: "PageRegister",
   components: {
     Heading,
+    BaseInput,
+  },
+  methods: {
+    DemoStart() {
+      this.$router.push("/");
+    },
   },
 };
 </script>
