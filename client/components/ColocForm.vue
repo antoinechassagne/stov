408b31470e7174b6b52c5d1f110a8675d4c74bef
Paul Riffard <paul.riffard@gmail.com>
2021-02-19 16:04:29 +0100

    début mise en forme ecran de création de colocation

--- client/components/ColocForm.vue
+++ client/components/ColocForm.vue
@@ -1,12 +1,19 @@
 <template>
-<div>
-  <h2>Create Coloc Form work</h2>
-    <form>
-    <label>Rentrer le nom de votre coloc</label>
-    <input type="text" v-model="colocName">
-    <button v-if="colocName" @click.prevent="successCreate()" > Créer la collocation </button> 
-  </form>
+<div class="h-screen max-h-screen ">
 
+<div class="h-full flex flex-col justify-evenly items-center text-center">
+<img  src="../assets/styles/images/mainrock.svg"/>
+
+  
+  <h2>Crée ta coloc !</h2>
+      <div> Pour commencer, il te faut un nom de colocation pour ton Støv </div>
+      <div>
+    <label>Choisis un nom stylé</label>
+    <input class="text-center" placeholder="La loft Story" type="text" v-model="colocName">
+      </div>
+    <button @click.prevent="successCreate()" > C'est partie ! </button> 
+
+</div>
 </div>
 </template>
 
