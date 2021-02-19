f4cebfc575972a04e94ac7dc9b03582de2bd424e
Antoine Chassagne <antoine.chassagne@me.com>
2021-02-19 16:44:31 +0100

    WIP tasks list

--- client/pages/routines/index.vue
+++ client/pages/routines/index.vue
@@ -5,28 +5,30 @@
 <script>
 export default {
   name: "RoutinePage",
-  props: {
-    taskToEdit: {
-      type: Object,
-    },
-  },
   data() {
     return {
+      taskExpandedId: null,
       tasks: [
-        { id: 1, label: "Vaisselle", userPseudo: "Paulo", weight: 2, daily: true },
-        { id: 2, label: "Nettoyer les WC", userPseudo: "Koueks", weight: 3, daily: false },
-        { id: 3, label: "Poussière", userPseudo: "Ludivine", weight: 1, daily: true },
-        { id: 4, label: "Aspirateur", userPseudo: "Jessica", weight: 1, daily: false },
-        { id: 5, label: "Calcaire salle de bain", userPseudo: "Teddy", weight: 3, daily: false },
+        { id: 1, label: "Vaisselle", userPseudo: "Paulo", weight: 2, daily: true, completed: false },
+        { id: 2, label: "Nettoyer les WC", userPseudo: "Koueks", weight: 3, daily: false, completed: false },
+        { id: 3, label: "Poussière", userPseudo: "Ludivine", weight: 1, daily: true, completed: false },
+        { id: 4, label: "Aspirateur", userPseudo: "Jessica", weight: 1, daily: false, completed: false },
+        { id: 5, label: "Calcaire salle de bain", userPseudo: "Teddy", weight: 3, daily: false, completed: false },
         {
           id: 6,
           label: "Nettoyer les crottes de pigeon sur le balcon",
           userPseudo: "Antoine",
           weight: 3,
           daily: false,
+          completed: false,
         },
       ],
     };
+  },
+  created() {
+    if (this.$route.query.taskId) {
+      this.taskExpandedId = this.$route.query.taskId;
+    }
   },
 };
 </script>
--- client/pages/tasks/index.vue
+++ client/pages/tasks/index.vue
@@ -4,37 +4,47 @@
     <Heading level="2">Cette semaine</Heading>
     <ul>
       <li v-for="(task, index) in weekTasks" :key="index">
-        {{ task.label }} - {{ getWeightLabel(task.weight) }} - {{ task.userPseudo }}
+        {{ task.label }} - {{ getWeightLabel(task.weight) }} - {{ task.userPseudo }} - {{ task.completed }}
       </li>
     </ul>
     <Heading level="2">Aujourd'hui</Heading>
     <ul>
       <li v-for="(task, index) in todayTasks" :key="index">
-        {{ task.label }} - {{ getWeightLabel(task.weight) }} - {{ task.userPseudo }}
+        {{ task.label }} - {{ getWeightLabel(task.weight) }} - {{ task.userPseudo }} - {{ task.completed }}
       </li>
     </ul>
     <button class="rounded-full h-12 bg-blue-100 p-2 text-white" @click="goToCreateTaskPage">Nouvelle tâche</button>
+    <SwipableTasksList :tasks="tasks" @updateTaskState="updateTaskState" />
   </fragment>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import Heading from "@/components/texts/Heading";
+import SwipableTasksList from "@/components/tasks/SwipableTasksList";
 
 export default {
   name: "PageHome",
   components: {
     Heading,
+    SwipableTasksList,
   },
   data() {
     return {
       tasks: [
-        { label: "Vaisselle", userPseudo: "Paulo", weight: 2, daily: true },
-        { label: "Nettoyer les WC", userPseudo: "Koueks", weight: 3, daily: false },
-        { label: "Poussière", userPseudo: "Ludivine", weight: 1, daily: true },
-        { label: "Aspirateur", userPseudo: "Jessica", weight: 1, daily: false },
-        { label: "Calcaire salle de bain", userPseudo: "Teddy", weight: 3, daily: false },
-        { label: "Nettoyer les crottes de pigeon sur le balcon", userPseudo: "Antoine", weight: 3, daily: false },
+        { id: 1, label: "Vaisselle", userPseudo: "Paulo", weight: 2, daily: true, completed: false },
+        { id: 2, label: "Nettoyer les WC", userPseudo: "Koueks", weight: 3, daily: false, completed: false },
+        { id: 3, label: "Poussière", userPseudo: "Ludivine", weight: 1, daily: true, completed: false },
+        { id: 4, label: "Aspirateur", userPseudo: "Jessica", weight: 1, daily: false, completed: false },
+        { id: 5, label: "Calcaire salle de bain", userPseudo: "Teddy", weight: 3, daily: false, completed: false },
+        {
+          id: 6,
+          label: "Nettoyer les crottes de pigeon sur le balcon",
+          userPseudo: "Antoine",
+          weight: 3,
+          daily: false,
+          completed: false,
+        },
       ],
     };
   },
@@ -63,6 +73,13 @@ export default {
     },
     goToCreateTaskPage() {
       this.$router.push("/tasks/create");
+    },
+    updateTaskState(task) {
+      // FIXME :(
+      task.completed = !task.completed;
+      const taskIndex = this.tasks.find((t) => t.id === task.id);
+      this.tasks.slice(taskIndex, 1);
+      this.tasks.push(task);
     },
   },
 };
