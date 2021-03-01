63304fd19b8435678a5f979fab25f5496c8877fb
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-03-01 19:47:50 +0100

    Wip new task, change style for radio buttons

--- components/RadioButton.vue
+++ components/RadioButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inline-block rounded-2xl bg-white checked:bg-yellow-100 border-2 border-black h-8 checked:bg-yellow-100 checked:border-transparent font-semibold text-center text-black w-40">
+  <div class="inline-block rounded-2xl bg-white checked:bg-yellow-100 border-2 border-gray-70 h-8 checked:bg-yellow-100 checked:border-transparent font-semibold text-center text-black py-1">
     <input type="radio" :name="name" class="hidden">
     <label> {{label}} </label>
   </div>
--- pages/tasks/Create.vue
+++ pages/tasks/Create.vue
@@ -4,17 +4,47 @@
       <div class="max-w-md mx-auto">
         <h1 class="font-bold text-2xl text-center">Nouvelle tâche</h1>
         <div class="mx-5">
-          <div class="bg-blue-10">
-            <p class="p-4">
-              Choisis <b>un nom de tâche clair</b> pour que tout le monde le comprenne ; tu peux aussi donner un nom très personnalisé avec tes propres termes !
-            </p>
+
+          <div id="etape-1">
+            <div class="bg-blue-10">
+              <p class="p-4">
+                Choisis <b>un nom de tâche clair</b> pour que tout le monde le comprenne ; tu peux aussi donner un nom très personnalisé avec tes propres termes !
+              </p>
+            </div>
+            <BaseInput label="Nom de la tâche" placeholder="Passer le balais" type="text" class="pb-3"/>
+            <ButtonSecondary>Valider le nom</ButtonSecondary>
           </div>
-          <BaseInput label="Nom de la tâche" placeholder="Passer le balais" type="text"></BaseInput>
-          <ButtonTransparent>Valider le nom</ButtonTransparent>
-          <Button>Créer la tâche ménagère</Button>
+
+          <div id="etape-2">
+            <div class="bg-blue-10">
+              <p class="p-4">
+                Ensuite, choisis la <b>difficulté</b> de cette tâche :  c’est à toi de la définir en fonction du logement ou de le pénibilité que vous associez à cette tâche.
+              </p>
+            </div>
+            <label for="difficulty">Difficulté de la tâche</label>
+            <div class="flex space-x-5">
+              <RadioButton label="Facile" name="difficulty" class="px-6"/>
+              <RadioButton label="Moyen" name="difficulty" class="px-6"/>
+              <RadioButton label="Difficile" name="difficulty" class="px-6"/>
+            </div>
+
+
+            <div id="etape-3">
+              <div class="bg-blue-10">
+                <p class="p-4">
+                  Enfin, choisis sa <b>récurrence</b> : elle te permet de différencier les tâches récurrentes quotidiennes (au moins une fois par jour) des tâches occassionnelles hebdomadaires (maximum une fois par semaine).                </p>
+              </div>
+              <label for="recurrency">Récurrence de la tâche</label>
+              <div class="flex space-x-5">
+                <RadioButton label="Quotidienne" name="recurrency" class="px-8"/>
+                <RadioButton label="Hebdomadaire" name="recurrency" class="px-8"/>
+              </div>
+            </div>
+          </div>
+
+          <ButtonPrimary>Créer la tâche ménagère</ButtonPrimary>
         </div>
       </div>
-      <ButtonPrimary>Valider</ButtonPrimary>
     </div>
 
     <!--
